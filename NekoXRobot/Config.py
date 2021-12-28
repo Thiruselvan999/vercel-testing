9439cfe1f0c4d74fd8180aa6b6534ffd02b25dd9
HACKER <89743468+Thiruselvan999@users.noreply.github.com>
2021-12-28 21:23:41 +0530

    Update Config.py

--- NekoXRobot/Config.py
+++ NekoXRobot/Config.py
@@ -26,7 +26,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/mrjoker/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/NekoXRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
